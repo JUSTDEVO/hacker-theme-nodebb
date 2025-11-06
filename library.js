37f44881e6608f2126a3a88260a04cf2197d7d91
JUSTDEVO <devoloko4710@gmail.com>
2025-11-06 04:48:29 +0300

    Add files via upload

--- library.js
+++ library.js
@@ -1,85 +1,188 @@
-const meta = require.main.require("./src/meta")
-const user = require.main.require("./src/user")
+'use strict';
 
-const library = module.exports
+const nconf = require.main.require('nconf');
+const meta = require.main.require('./src/meta');
+const _ = require.main.require('lodash');
+const user = require.main.require('./src/user');
 
-library.init = async (params) => {
-  const { router, middleware } = params
+const controllers = require('./lib/controllers');
 
-  // Add custom routes if needed
-  router.get("/admin/plugins/hacker", middleware.admin.buildHeader, renderAdmin)
-  router.get("/api/admin/plugins/hacker", renderAdmin)
+const library = module.exports;
+
+const defaults = {
+	enableQuickReply: 'on',
+	enableBreadcrumbs: 'on',
+	centerHeaderElements: 'off',
+	mobileTopicTeasers: 'off',
+	stickyToolbar: 'on',
+	topicSidebarTools: 'on',
+	topMobilebar: 'off',
+	autohideBottombar: 'on',
+	openSidebars: 'off',
+	chatModals: 'off',
+};
+
+library.init = async function (params) {
+	const { router, middleware } = params;
+	const routeHelpers = require.main.require('./src/routes/helpers');
+
+	routeHelpers.setupAdminPageRoute(router, '/admin/plugins/harmony', [], controllers.renderAdminPage);
+
+	routeHelpers.setupPageRoute(router, '/user/:userslug/theme', [
+		middleware.exposeUid,
+		middleware.ensureLoggedIn,
+		middleware.canViewUsers,
+		middleware.checkAccountPermissions,
+	], controllers.renderThemeSettings);
+
+	if (nconf.get('isPrimary') && process.env.NODE_ENV === 'production') {
+		setTimeout(buildSkins, 0);
+	}
+};
+
+async function buildSkins() {
+	try {
+		const plugins = require.main.require('./src/plugins');
+		await plugins.prepareForBuild(['client side styles']);
+		for (const skin of meta.css.supportedSkins) {
+			// eslint-disable-next-line no-await-in-loop
+			await meta.css.buildBundle(`client-${skin}`, true);
+		}
+		require.main.require('./src/meta/minifier').killAll();
+	} catch (err) {
+		console.error(err.stack);
+	}
 }
 
-async function renderAdmin(req, res) {
-  res.render("admin/plugins/hacker", {
-    title: "Hacker Theme Settings",
-  })
-}
-
-library.addAdminNavigation = async (header) => {
-  header.plugins.push({
-    route: "/plugins/hacker",
-    icon: "fa-terminal",
-    name: "Hacker Theme",
-  })
-  return header
-}
-
-library.defineWidgetAreas = async (areas) => {
-  const locations = ["header", "sidebar", "footer"]
-  const templates = [
-    "categories.tpl",
-    "category.tpl",
-    "topic.tpl",
-    "users.tpl",
-    "unread.tpl",
-    "recent.tpl",
-    "popular.tpl",
-    "top.tpl",
-    "tags.tpl",
-  ]
-
-  templates.forEach((template) => {
-    locations.forEach((location) => {
-      areas.push({
-        name: `${template.split(".")[0]} ${location}`,
-        template: template,
-        location: location,
-      })
-    })
-  })
-
-  return areas
-}
-
-library.getThemeConfig = async (config) => {
-  config.hideSubCategories = false
-  config.hideEmpties = false
-  config.enableQuickReply = true
-  return config
-}
-
-library.getAdminSettings = async (data) => {
-  return data
-}
-
-library.saveUserSettings = async (data) => {
-  return data
-}
+library.addAdminNavigation = async function (header) {
+	header.plugins.push({
+		route: '/plugins/harmony',
+		icon: 'fa-paint-brush',
+		name: '[[themes/harmony:theme-name]]',
+	});
+	return header;
+};
 
 library.addProfileItem = async (data) => {
-  data.links.push({
-    id: "theme",
-    route: "theme",
-    icon: "fa-paint-brush",
-    name: "Theme Settings",
-    public: false,
-  })
-  return data
-}
+	data.links.push({
+		id: 'theme',
+		route: 'theme',
+		icon: 'fa-paint-brush',
+		name: '[[themes/harmony:settings.title]]',
+		visibility: {
+			self: true,
+			other: false,
+			moderator: false,
+			globalMod: false,
+			admin: false,
+		},
+	});
 
-library.filterMiddlewareRenderHeader = async (data) => {
-  data.templateData.bootswatchSkin = ""
-  return data
-}
+	return data;
+};
+
+library.defineWidgetAreas = async function (areas) {
+	const locations = ['header', 'sidebar', 'footer'];
+	const templates = [
+		'categories.tpl', 'category.tpl', 'topic.tpl', 'users.tpl',
+		'unread.tpl', 'recent.tpl', 'popular.tpl', 'top.tpl', 'tags.tpl', 'tag.tpl',
+		'login.tpl', 'register.tpl', 'world.tpl',
+	];
+	function capitalizeFirst(str) {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	}
+	templates.forEach((template) => {
+		locations.forEach((location) => {
+			areas.push({
+				name: `${capitalizeFirst(template.split('.')[0])} ${capitalizeFirst(location)}`,
+				template: template,
+				location: location,
+			});
+		});
+	});
+
+	areas = areas.concat([
+		{
+			name: 'Main post header',
+			template: 'topic.tpl',
+			location: 'mainpost-header',
+		},
+		{
+			name: 'Main post footer',
+			template: 'topic.tpl',
+			location: 'mainpost-footer',
+		},
+		{
+			name: 'Sidebar Footer',
+			template: 'global',
+			location: 'sidebar-footer',
+		},
+		{
+			name: 'Brand Header',
+			template: 'global',
+			location: 'brand-header',
+		},
+		{
+			name: 'About me (before)',
+			template: 'account/profile.tpl',
+			location: 'profile-aboutme-before',
+		},
+		{
+			name: 'About me (after)',
+			template: 'account/profile.tpl',
+			location: 'profile-aboutme-after',
+		},
+	]);
+
+	return areas;
+};
+
+library.loadThemeConfig = async function (uid) {
+	const [themeConfig, userConfig] = await Promise.all([
+		meta.settings.get('harmony'),
+		user.getSettings(uid),
+	]);
+
+	const config = { ...defaults, ...themeConfig, ...(_.pick(userConfig, Object.keys(defaults))) };
+	config.enableQuickReply = config.enableQuickReply === 'on';
+	config.enableBreadcrumbs = config.enableBreadcrumbs === 'on';
+	config.centerHeaderElements = config.centerHeaderElements === 'on';
+	config.mobileTopicTeasers = config.mobileTopicTeasers === 'on';
+	config.stickyToolbar = config.stickyToolbar === 'on';
+	config.topicSidebarTools = config.topicSidebarTools === 'on';
+	config.autohideBottombar = config.autohideBottombar === 'on';
+	config.topMobilebar = config.topMobilebar === 'on';
+	config.openSidebars = config.openSidebars === 'on';
+	config.chatModals = config.chatModals === 'on';
+	return config;
+};
+
+library.getThemeConfig = async function (config) {
+	config.theme = await library.loadThemeConfig(config.uid);
+	config.openDraftsOnPageLoad = false;
+	return config;
+};
+
+library.getAdminSettings = async function (hookData) {
+	if (hookData.plugin === 'harmony') {
+		hookData.values = {
+			...defaults,
+			...hookData.values,
+		};
+	}
+	return hookData;
+};
+
+library.saveUserSettings = async function (hookData) {
+	Object.keys(defaults).forEach((key) => {
+		if (hookData.data.hasOwnProperty(key)) {
+			hookData.settings[key] = hookData.data[key] || undefined;
+		}
+	});
+	return hookData;
+};
+
+library.filterMiddlewareRenderHeader = async function (hookData) {
+	hookData.templateData.bootswatchSkinOptions = await meta.css.getSkinSwitcherOptions(hookData.req.uid);
+	return hookData;
+};
